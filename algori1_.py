b74a8ce4052979f4af1eed2e59ad6f2dbe35125b
Gold-gyu <lkg2097@naver.com>
2023-01-12 15:28:37 +0900

    알고리즘1

---
+++ algori1_.py
@@ -0,0 +1,13 @@
+T = int(input())
+ 
+for tc in range(1, T+1):
+    num_list = list(map(int,input().split()))
+ 
+    ans = 0
+ 
+ 
+    for num in num_list:
+        if num % 2 == 1:
+            ans = ans + num
+ 
+    print(f"#{tc} {ans}")
